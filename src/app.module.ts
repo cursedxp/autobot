b73f82029b98305487be67b590f5927484ce6d68
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-08 00:23:33 +0200

    🔧 chore(app.module.ts): add OrderService and BinanceService to the providers array to enable their usage in the application ✨ feat(app.module.ts): import and include OrderModule in the imports array to enable usage of OrderModule in the application 🔧 chore(app.module.ts): set isGlobal option to true in ConfigModule.forRoot() to make the configuration module available globally in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,19 @@ import { PrismaService } from './prisma/prisma.service';
 import { HealthController } from './health/health.controller';
 import { PriceService } from './price/price.service';
 import { ConfigModule } from '@nestjs/config';
+import { OrderService } from './order/order.service';
+import { OrderModule } from './order/order.module';
+import { BinanceService } from './binance/binance.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), OrderModule],
   controllers: [AppController, HealthController],
-  providers: [AppService, PrismaService, PriceService],
+  providers: [
+    AppService,
+    PrismaService,
+    PriceService,
+    OrderService,
+    BinanceService,
+  ],
 })
 export class AppModule {}
