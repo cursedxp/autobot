299abe1e52299e90e87f6d9ebe21aa88d661c42f
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-07 22:55:29 +0200

    🔥 chore(app): remove unused app.controller.spec.ts file 🔧 chore(app): add ConfigModule.forRoot() to app.module.ts to enable configuration loading from environment variables ✨ feat(app): add PriceService to providers in app.module.ts to enable price-related functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { HealthController } from './health/health.controller';
+import { PriceService } from './price/price.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, HealthController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, PriceService],
 })
 export class AppModule {}
