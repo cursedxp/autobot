56c4c06c73daf6f664ad339b26a05f1cee4b60f8
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-08 22:58:03 +0200

    🚀 feat(app.module.ts): add ProfitController and ProfitService to AppModule to enable profit calculation functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,19 @@ import { ConfigModule } from '@nestjs/config';
 import { OrderService } from './order/order.service';
 import { OrderModule } from './order/order.module';
 import { BinanceService } from './binance/binance.service';
+import { ProfitService } from './profit/profit.service';
+import { ProfitController } from './profit/profit.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), OrderModule],
-  controllers: [AppController, HealthController],
+  controllers: [AppController, HealthController, ProfitController],
   providers: [
     AppService,
     PrismaService,
     PriceService,
     OrderService,
     BinanceService,
+    ProfitService,
   ],
 })
 export class AppModule {}
