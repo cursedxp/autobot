369c9d1e0d5266c18e2e709d2625077927423648
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-08 17:05:55 +0200

    🔧 fix(order.service.ts): implement OnModuleInit interface to initialize starting order on module initialization ✨ feat(order.service.ts): add initializeOrder method to create a starting order if it doesn't exist for the specified symbol

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,9 +3,10 @@ import { Cron } from '@nestjs/schedule';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { BinanceService } from 'src/binance/binance.service';
+import { OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class OrderService {
+export class OrderService implements OnModuleInit {
   private readonly logger = new Logger(OrderService.name);
 
   constructor(
@@ -14,11 +15,39 @@ export class OrderService {
     private binanceService: BinanceService,
   ) {}
 
-  @Cron('45 * * * * *')
+  @Cron('20 * * * * *')
   handlePriceAdjustment() {
     const assetSymbol = this.config.get('ASSET_SYMBOL');
     this.adjustStopOrder(assetSymbol);
     this.logger.debug(`Checked and adjusted stop order for ${assetSymbol}`);
+  }
+  async onModuleInit() {
+    await this.initializeOrder('BTCUSDT'); // or any other symbol you're tracking
+  }
+
+  async initializeOrder(symbol: string): Promise<void> {
+    const existingOrder = await this.prisma.order.findFirst({
+      where: { symbol, status: 'OPEN' },
+    });
+
+    if (!existingOrder) {
+      // Fetch the current price from Binance (or your AssetPrice model)
+      const currentPrice = await this.binanceService.getCurrentPrice(symbol); // You'll need to implement this function in your Binance service
+
+      await this.prisma.order.create({
+        data: {
+          symbol: symbol,
+          price: currentPrice,
+          type: 'STOP_LIMIT', // or whatever type you want to initialize with
+          status: 'OPEN',
+          // If you have an actual order on Binance, you'd fetch and save the Binance order ID here
+          // binanceOrderId: 'some_order_id_from_binance',
+        },
+      });
+      this.logger.log(
+        `Initialized starting order for ${symbol} at price ${currentPrice}`,
+      );
+    }
   }
 
   async adjustStopOrder(symbol: string): Promise<void> {
